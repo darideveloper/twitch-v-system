aeced860575822c14c7dcedd910b5746ad34181c
daridev <darideveloper@gmail.com>
2023-08-10 12:16:08 -0600

    refactor (comments): rename variables

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
-from comments import models
 from core.views import BaseJsonGetView, BaseJsonGetDisableView
+from comments import models
 from core.decorators import validate_token
 
 class Bots (BaseJsonGetDisableView):
@@ -18,27 +18,27 @@ class CommentsPhantom (BaseJsonGetView):
     @validate_token
     def get (self, request):
         
-        data = self.get_data()
+        comments = self.get_data()
         
         # Get only mods unique comments abd filter active
-        mods_comments = list(set(map (lambda row: row.comment_mod, data)))
+        mods_comments = list(set(map (lambda row: row.comment_mod, comments)))
         mods_comments = list(filter(lambda row: row.is_active, mods_comments))
         
         # Get bots comments to each mod
-        data_list = {}
+        comments_relation = {}
         for comment_mod in mods_comments:
             
             # Get comments and filter only active comments
-            bots_comments = data.filter (comment_mod=comment_mod)
+            bots_comments = comments.filter (comment_mod=comment_mod)
             bots_comments = list(map (lambda row: row.comment_res, bots_comments))
             bots_comments = list(filter(lambda row: row.is_active, bots_comments))
             
             # Get only comments text
             bots_comments = list(map (lambda row: row.comment, bots_comments))
             
-            data_list[comment_mod.comment] = bots_comments
+            comments_relation[comment_mod.comment] = bots_comments
             
-        if not data_list:
+        if not comments_relation:
             return JsonResponse({
                 'status': 'error',
                 'message': 'No data found',
@@ -48,5 +48,5 @@ class CommentsPhantom (BaseJsonGetView):
         return JsonResponse({
             'status': 'ok',
             'message': 'Data found',
-            'data': data_list
+            'data': comments_relation
         })
