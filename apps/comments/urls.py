064e845017ad76c6c8ac1fc3e316aa1f436882e4
daridev <darideveloper@gmail.com>
2023-08-13 22:44:51 -0600

    feat (comments/views): remove phantom comments

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path ('bots/', views.Bots.as_view(), name='bots'),
     path ('comments/', views.Comments.as_view(), name='comments'),
     path ('mods/', views.Mods.as_view(), name='mods'),
-    path ('comments-phantom/', views.CommentsPhantom.as_view(), name='comments-phantom'),
 ]
 
--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,7 +1,5 @@
-from django.http import JsonResponse
 from core.views import BaseJsonGetView, BaseJsonGetDisableView
 from comments import models
-from core.decorators import validate_token
 
 class Bots (BaseJsonGetDisableView):
     model = models.Bot     
@@ -14,39 +12,3 @@ class Mods (BaseJsonGetView):
     
 class CommentsPhantom (BaseJsonGetView):
     model = models.Comment
-    
-    @validate_token
-    def get (self, request):
-        
-        comments = self.get_data()
-        
-        # Get only mods unique comments abd filter active
-        mods_comments = list(set(map (lambda row: row.comment_mod, comments)))
-        mods_comments = list(filter(lambda row: row.is_active, mods_comments))
-        
-        # Get bots comments to each mod
-        comments_relation = {}
-        for comment_mod in mods_comments:
-            
-            # Get comments and filter only active comments
-            bots_comments = comments.filter (comment_mod=comment_mod)
-            bots_comments = list(map (lambda row: row.comment_res, bots_comments))
-            bots_comments = list(filter(lambda row: row.is_active, bots_comments))
-            
-            # Get only comments text
-            bots_comments = list(map (lambda row: row.comment, bots_comments))
-            
-            comments_relation[comment_mod.comment] = bots_comments
-            
-        if not comments_relation:
-            return JsonResponse({
-                'status': 'error',
-                'message': 'No data found',
-                'data': []
-            })
-        
-        return JsonResponse({
-            'status': 'ok',
-            'message': 'Data found',
-            'data': comments_relation
-        })
