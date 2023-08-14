96223a6e33fdd0ec79a910ae334c94e89ae8c81b
daridev <darideveloper@gmail.com>
2023-08-14 13:25:07 -0600

    test (viwers/TestBotsView): add test_invalid_token_delete

--- apps/viwers/tests.py
+++ apps/viwers/tests.py
@@ -1,26 +1,29 @@
-# from viwers import models as viwers_models
-# from core.tests import BaseTestApi
+from viwers import models as viwers_models
+from core.tests import BaseTestApi
 
-# API_BASE = "viwers"
-# TOKEN = "token123*"
+API_BASE = "viwers"
+TOKEN = "token123*"
 
-# class TestBotsView (BaseTestApi): 
+class TestBotsView (BaseTestApi): 
        
-#     # Setup test 
-#     api_base = API_BASE
-#     endpoint = "bots"
-#     token = TOKEN
-#     model = viwers_models.Bot
-#     auto_generate_data = True
+    # Setup test 
+    api_base = API_BASE
+    endpoint = "bots"
+    token = TOKEN
+    model = viwers_models.Bot
+    auto_generate_data = True
     
-#     def test_invalid_token (self):
-#         self.base_invalid_token ()
+    def test_invalid_token (self):
+        self.base_invalid_token ()
+        
+    def test_invalid_token_delete (self):
+        self.base_invalid_token ("delete")
     
-#     def test_disable (self): 
-#         self.base_disable ()
+    def test_disable (self): 
+        self.base_disable ()
  
-#     def test_get_no_registers (self): 
-#         self.base_get_no_registers ()
+    def test_get_no_registers (self): 
+        self.base_get_no_registers ()
   
-#     def test_get (self):
-#         self.base_get ()
+    def test_get (self):
+        self.base_get ()
